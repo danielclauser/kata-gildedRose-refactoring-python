6119b8994f52052996e2ec1ab9f7866214699616
danielclauser <clauser.daniel@gmail.com>
2025-01-27 11:14:11 +0100

    Add constants for special items

--- python/src/gilded_rose/gilded_rose.py
+++ python/src/gilded_rose/gilded_rose.py
@@ -1,31 +1,32 @@
 class GildedRose:
+    ITEMS_LEGENDARY = {"Sulfuras, Hand of Ragnaros"}
+    ITEMS_QUALITY_ENHANCED_ON_DEGRADE = {"Backstage passes to a TAFKAL80ETC concert"}
+    ITEMS_QUALITY_ONLY_UP_ON_DEGRADE = {"Aged Brie"}
+    items_no_degrade = ITEMS_QUALITY_ONLY_UP_ON_DEGRADE
+    items_no_degrade = items_no_degrade.union(ITEMS_LEGENDARY)
+    items_no_degrade = items_no_degrade.union(ITEMS_QUALITY_ENHANCED_ON_DEGRADE)
+
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name not in {
-                "Aged Brie",
-                "Backstage passes to a TAFKAL80ETC concert",
-            }:
-                if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name not in self.items_no_degrade:
+                if item.quality > 0:
                     item.quality = item.quality - 1
             elif item.quality < 50:
                 item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.name in self.ITEMS_QUALITY_ENHANCED_ON_DEGRADE:
                     if item.sell_in < 11 and item.quality < 50:
                         item.quality = item.quality + 1
                     if item.sell_in < 6 and item.quality < 50:
                         item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name not in self.ITEMS_LEGENDARY:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if (
-                            item.quality > 0
-                            and item.name != "Sulfuras, Hand of Ragnaros"
-                        ):
+                if item.name not in self.ITEMS_QUALITY_ONLY_UP_ON_DEGRADE:
+                    if item.name not in self.ITEMS_QUALITY_ENHANCED_ON_DEGRADE:
+                        if item.quality > 0 and item.name not in self.ITEMS_LEGENDARY:
                             item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
