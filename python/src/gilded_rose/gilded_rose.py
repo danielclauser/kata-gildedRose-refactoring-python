f2e787a7623fa784d83a7b4207a66016faab668d
danielclauser <clauser.daniel@gmail.com>
2025-01-27 15:42:02 +0100

    refactoring GildedRose for more readable conditions

--- python/src/gilded_rose/gilded_rose.py
+++ python/src/gilded_rose/gilded_rose.py
@@ -1,13 +1,17 @@
-class Item:
+class SpecialProperiesItem:
     QUALITY_ONLY_UP_ON_DEGRADE: set[str] = {"Aged Brie"}
     QUALITY_ENHANCED_ON_DEGRADE: set[str] = {
         "Backstage passes to a TAFKAL80ETC concert"
     }
     QUALITY_SELL_IN_LOCKED: set[str] = {"Sulfuras, Hand of Ragnaros"}
     QUALITY_DOUBLE_VARIATION: set[str] = {"Conjured Mana Cake"}
+
+
+class Item:
     name: str
     sell_in: int
     sell_in_variation_units: int = -1
+    sell_in_set_zero_when_expired: bool = False
     quality: int
     quality_up_on_degrade: bool = False
     quality_no_degrade: bool = False
@@ -23,14 +27,15 @@ class Item:
         self.quality = quality
 
     def _set_info_item_by_name(self):
-        if self.name in self.QUALITY_ONLY_UP_ON_DEGRADE:
+        if self.name in SpecialProperiesItem.QUALITY_ONLY_UP_ON_DEGRADE:
             self.quality_up_on_degrade = True
             self.quality_variaton_units = 1
-        if self.name in self.QUALITY_SELL_IN_LOCKED:
+        if self.name in SpecialProperiesItem.QUALITY_SELL_IN_LOCKED:
             self.quality_sell_in_locked = True
-        if self.name in self.QUALITY_ENHANCED_ON_DEGRADE:
+        if self.name in SpecialProperiesItem.QUALITY_ENHANCED_ON_DEGRADE:
             self.quality_enhanced_near_expiration = True
-        if self.name in self.QUALITY_DOUBLE_VARIATION:
+            self.sell_in_set_zero_when_expired = True
+        if self.name in SpecialProperiesItem.QUALITY_DOUBLE_VARIATION:
             self.quality_variaton_units *= 2
         if (
             self.quality_enhanced_near_expiration
@@ -55,22 +60,17 @@ class GildedRose:
         self.items = items
 
     def _item_is_expired(self, item: Item) -> None:
-        if item.quality_up_on_degrade and item.quality < self.MAX_ITEM_QUALITY:
-            item.quality += item.quality_variaton_units
-        elif item.quality_enhanced_near_expiration:
-            item.quality = 0
-        elif not item.quality_no_degrade and item.quality > 0:
-            item.quality += item.quality_variaton_units
+        if item.check_is_expired():
+            if item.quality < self.MAX_ITEM_QUALITY and item.quality > 0:
+                item.quality += item.quality_variaton_units
+            elif item.sell_in_set_zero_when_expired:
+                item.quality = 0
 
     def _item_update_sell_in(self, item: Item) -> None:
-        if not item.quality_sell_in_locked:
-            item.sell_in += item.sell_in_variation_units
+        item.sell_in += item.sell_in_variation_units
 
     def _item_update_quality(self, item: Item) -> None:
-        if not item.quality_no_degrade:
-            if item.quality > 0:
-                item.quality += item.quality_variaton_units
-        elif item.quality < self.MAX_ITEM_QUALITY:
+        if item.quality > 0 or item.quality_up_on_degrade:
             item.quality += item.quality_variaton_units
             if item.quality_enhanced_near_expiration:
                 if item.sell_in < 11 and item.quality < self.MAX_ITEM_QUALITY:
@@ -80,8 +80,9 @@ class GildedRose:
 
     def run_items_day_passed_event(self) -> None:
         for item in self.items:
-            self._item_update_quality(item)
+            if item.quality_sell_in_locked:
+                continue
+            if item.quality < self.MAX_ITEM_QUALITY:
+                self._item_update_quality(item)
             self._item_update_sell_in(item)
-            item.check_is_expired()
-            if item.is_expired:
-                self._item_is_expired(item)
+            self._item_is_expired(item)
